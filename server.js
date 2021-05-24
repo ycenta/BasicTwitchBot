3403cfb89b5f6f64d29439e6b6ddd61de1ce598e
ycenta <40060168+ycenta@users.noreply.github.com>
2021-05-24 17:42:25 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,42 @@
+require('dotenv').config(); 
+
+const tmi = require('tmi.js');
+
+const client = new tmi.Client({
+	options: { debug: true },
+	identity: {
+		username: process.env.TWITCH_BOT_NAME,
+		password: process.env.TWITCH_OAUTH_TOKEN
+	},
+	channels: [ 'your channel name' ]
+});
+
+
+client.connect();
+
+client.on('message', (channel, tags, message, self) => {
+	if(self || !message.startsWith('!')) return;
+
+        const args = message.slice(1).split(' ');
+        const command = args.shift();
+        //console.log(args);
+
+
+    if(command === 'SimpleCommand') {
+		client.say(channel, `simple answer`);
+	}
+    if(command === 'CommandsWithArguments') {
+            if(args[0]){
+		client.say(channel, `${args[0]} est CRINGE`);
+	    }
+    }
+
+	
+});
+	
+
+//https://static-cdn.jtvnw.net/emoticons/v1/nom_de_lemote/1.0
+
+
+
+//https://static-cdn.jtvnw.net/emoticons/v1/58765/1.0
